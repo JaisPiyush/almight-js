1042ba8dec96c32cd28ddbfc2ebb90672efa2f1a
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2022-04-14 00:11:54 +0530

    Added TImeBoundPromise class

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,43 @@
 export * from "./storage";
 export * from "./functions";
+
+export class TimeBoundPromise<T> extends Promise<T> {
+    constructor(timeout, callback, time_out_error?: Error) {
+        // We need to support being called with . milliseconds
+        // value, because the various Promise methods (`then` and
+        // such) correctly call the subclass constructor when
+        // building the new promises they return.
+        const haveTimeout = typeof timeout === "number";
+        const init = haveTimeout ? callback : timeout;
+        super((resolve, reject) => {
+            if (haveTimeout) {
+                const timer = setTimeout(() => {
+                    const error = time_out_error !== undefined ? time_out_error:  new Error(`Promise timed out after ${timeout}ms`);
+                    reject(error);
+                }, timeout);
+                init(
+                    (value) => {
+                        clearTimeout(timer);
+                        resolve(value);
+                    },
+                    (error) => {
+                        clearTimeout(timer);
+                        reject(error);
+                    }
+                );
+            } else {
+                init(resolve, reject);
+            }
+        });
+    }
+    
+    // Timebounding already existing promises
+    static resolveWithTimeout(timeout, promise, time_out_error?: Error) {
+        if (!promise || typeof promise.then !== "function") {
+            // `promise` isn't a thenable, no need for the timeout,
+            // fulfill immediately
+            return this.resolve(promise);
+        }
+        return new this(timeout, promise.then.bind(promise), time_out_error);
+    }
+}
