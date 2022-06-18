a219d57b83c73ae0d08f3ab45b1477b00853d476
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2022-06-18 18:28:55 +0530

    Test setup completed

--- packages/utils/__tests__/functions.spec.ts
+++ packages/utils/__tests__/functions.spec.ts
@@ -1,6 +1,5 @@
 import {  isWebPlatform } from "../src"
 import { expect } from "chai"
-import {findEnv, configEnv} from "../src/utility"
 
 
 describe("Testing isWebPlatform function", () => {
@@ -17,26 +16,6 @@ describe("Testing isWebPlatform function", () => {
     });
 
 
-    describe("findEnv", () => {
-        it("findEnv success in finding .env file", () => {
-            const filePath = findEnv(__dirname);
-            expect(filePath).not.to.equal(__dirname);
-        });
-        it("findEnv unable to find any env due to low stop_limit and thus returning dirname", () => {
-            process.env["STOP_LIMIT"] = "1";
-            const newFilePath = findEnv(__dirname);
-            expect(newFilePath).to.equal(__dirname);
-            process.env["STOP_LIMIT"] = "4";
-        });
-    });
-
-    describe("configEnv", () => {
-        
-        it("configEnv able to find env variable in root of the project", () => {
-            const out = configEnv();
-            expect(process.env.NODE_ENV, findEnv(__dirname)).not.to.be.undefined;
-        })
-    })
 
 });
 
